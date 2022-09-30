73405a99affbfe449817dd9e4ff2209ef69d18a4
RPE8 <vitali.shuman96@gmail.com>
2022-09-30 12:34:55 +0300

    Add debounced handler for search input live change

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,25 @@ import { Axios as axios } from 'axios';
 import './style.css';
 
 console.log(axios);
+
+const searchInput = document.querySelector('.search-field__search-input');
+
+function debounce(fn: (...args: unknown[]) => unknown, timeout = 300) {
+	let timer: number;
+	return (...args: unknown[]) => {
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			fn.apply(this, args);
+		}, timeout);
+	};
+}
+
+const onSearchInputLiveChange = (event: Event) => {
+	const target = event.target as HTMLInputElement;
+	const value = target.value;
+	console.log(value);
+};
+
+if (searchInput)
+	searchInput.addEventListener('input', debounce(onSearchInputLiveChange));
+
