a1dfa3763672c65079fcb8dea407a4cb1a9e8ce4
RPE8 <vitali.shuman96@gmail.com>
2022-10-02 15:37:57 +0300

    Handle search input events and add erro state

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import './style.css';
 
 console.log(axios);
 
-const searchInput = document.querySelector('.search-field__search-input');
+const searchInput = document.querySelector(
+	'.search-field__search-input'
+) as HTMLInputElement;
+const searchButton = document.querySelector(
+	'.search-field__search-button'
+) as HTMLButtonElement;
 
 function debounce(fn: (...args: unknown[]) => unknown, timeout = 300) {
 	let timer: number;
@@ -18,9 +23,41 @@ function debounce(fn: (...args: unknown[]) => unknown, timeout = 300) {
 const onSearchInputLiveChange = (event: Event) => {
 	const target = event.target as HTMLInputElement;
 	const value = target.value;
-	console.log(value);
+
+	if (value.trim()) {
+		removeError(searchInput);
+	}
+};
+
+const onSearchButtonClick = (event: Event): void => {
+	event.preventDefault();
+
+	if (!searchInput) return;
+
+	const value = searchInput.value.trim();
+	if (!value || !value.length) {
+		searchInput.setCustomValidity('Error');
+		showError(searchInput);
+	}
+
+	searchInput.setCustomValidity('');
+};
+
+const showError = (input: HTMLInputElement): void => {
+	if (!input) return;
+
+	input.classList.add('error');
+};
+
+const removeError = (input: HTMLInputElement): void => {
+	if (!input) return;
+
+	input.classList.remove('error');
 };
 
 if (searchInput)
 	searchInput.addEventListener('input', debounce(onSearchInputLiveChange));
 
+if (searchButton) {
+	searchButton.addEventListener('click', onSearchButtonClick);
+}
